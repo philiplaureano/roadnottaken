6ad676791bece84d6397ef2f4ecbe4d681dc0fe9
Philip Laureano <philip.laureano@gmail.com>
2020-07-10 18:44:43 +1000

    Added the Skipper project, which will modify a test assembly to do the test skipping

---
+++ Skipper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
