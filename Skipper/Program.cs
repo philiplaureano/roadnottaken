032a956d10774ab7f59b69f264808364c3efc28e
Philip Laureano <philip.laureano@gmail.com>
2020-07-11 16:01:07 +1000

    Added the skipper prototype code

--- Skipper/Program.cs
+++ Skipper/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using dnlib.DotNet;
+using Optional.Collections;
+using Optional.Unsafe;
+using Serilog;
+using Xunit;
 
 namespace Skipper
 {
@@ -6,7 +15,98 @@ namespace Skipper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel
+                .Warning()
+                .CreateLogger();
+
+            Parser.Default.ParseArguments<SkipperOptions>(args)
+                .WithParsed(options =>
+                {
+                    var assemblyLocation = options.TargetAssemblyLocation;
+                    var outputFile = options.OutputFile ?? assemblyLocation;
+                    var skipFilePath = options.SkipListFilePath;
+                    var skipReason = options.SkipReason ??
+                                     "This method has been skipped since it has been green in past runs";
+
+                    Log.Information($"Unit Test AssemblyLocation = {assemblyLocation} ");
+                    Log.Information($"Output Test Assembly Path = {outputFile}");
+                    Log.Information($"Skip File Path = {skipFilePath}");
+                    Log.Information($"Skip Reason = '{skipReason}'");
+
+                    var skipFileLines = File.ReadAllLines(skipFilePath);
+                    var skipFileEntries = new HashSet<string>(skipFileLines);
+
+                    Log.Debug($"{skipFileEntries.Count} Skip File Lines Loaded");
+
+                    bool ShouldSkipUnitTest(string assemblyName, string typeName, string methodName)
+                    {
+                        var combinedMethodName = $"{typeName}.{methodName}";
+                        return skipFileEntries.Contains(combinedMethodName);
+                    }
+
+                    Log.Information($"Reading unit test assembly file '{assemblyLocation}'");
+                    AddSkips(assemblyLocation, outputFile, skipReason, ShouldSkipUnitTest);
+                });
+        }
+
+        private static void AddSkips(string assemblyLocation, string outputFile, string skipReason,
+            Func<string, string, string, bool> testFilter)
+        {
+            var assemblyDef = AssemblyDef.Load(assemblyLocation);
+            var module = assemblyDef.Modules.First();
+            var types = module.Types.ToArray();
+            foreach (var type in types)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (!method.HasCustomAttributes)
+                        continue;
+
+                    var customAttributes = method.CustomAttributes.ToArray();
+
+                    // Search for the [Fact] or [Theory] attributes
+                    var factOrTheoryAttribute =
+                        customAttributes.FirstOrNone(c =>
+                            c?.AttributeType.Name == nameof(FactAttribute) ||
+                            c?.AttributeType.Name == nameof(TheoryAttribute));
+
+                    if (!factOrTheoryAttribute.HasValue)
+                    {
+                        Log.Verbose(
+                            $"Ignoring method '{method.FullName}' since it does not appear to be an xUnit test case");
+                        continue;
+                    }
+
+                    var targetAttribute = factOrTheoryAttribute.ValueOrFailure();
+
+                    // Ignore the method if it is already going to be skipped
+                    if (targetAttribute.HasNamedArguments && targetAttribute.NamedArguments.Any(c => c.Name == "Skip"))
+                    {
+                        Log.Warning(
+                            $"The test method '{method.FullName}' is already being skipped and won't be modified.");
+                        continue;
+                    }
+
+                    var assemblyName = type.DefinitionAssembly.Name;
+                    var typeName = type.Name;
+                    var methodName = method.Name;
+
+                    if (!testFilter(assemblyName, typeName, methodName))
+                    {
+                        Log.Warning(
+                            $"The test method '{method.FullName}' has not been marked for skipping and won't be modified.");
+                        continue;
+                    }
+
+                    targetAttribute.NamedArguments.Add(new CANamedArgument(false, module.CorLibTypes.String, "Skip",
+                        new CAArgument(module.CorLibTypes.String,
+                            skipReason)));
+                }
+            }
+
+            assemblyDef.Write(outputFile);
         }
     }
 }
---
+++ Skipper/SkipperOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace Skipper
+{
+    public class SkipperOptions
+    {
+        [Option('t',"targetTestAssembly", Required=true,HelpText = "The path of the test assembly that will be modified for skipping.")]
+        public string TargetAssemblyLocation { get; set; }
+        [Option('o',"outputFile", Required=false, HelpText = "The output path of the modified test assembly. If blank, then the Skipper will use the same path as the target test assembly path.")]
+        public string OutputFile { get; set; }
+        
+        [Option('s',"skipListFile", Required = true, HelpText = "The location of the skip list file that contains the list of single line test method entries that will be marked for skipping (in the form of Project.Namespace.TestType.TestMethodName, one entry per line).")]
+        public string SkipListFilePath { get; set; }
+        
+        [Option('r',"skipReason",Required = false, HelpText = "The reason why each one of the matching tests will be skipped. This text will become part of the text output when the tests are run and the matching tests are skipped.")]
+        public string SkipReason { get; set; }
+    }
+}
